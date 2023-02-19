7aaef0f38de8c79d5a7374f86ee8c73323af01a2
pejath <80777928+pejath@users.noreply.github.com>
2023-02-19 21:07:33 +0300

    fix

--- db/migrate/20230215110231_create_tests.rb
+++ db/migrate/20230215110231_create_tests.rb
@@ -6,7 +6,7 @@ class CreateTests < ActiveRecord::Migration[6.1]
       t.string :title, null: false
       t.integer :level, null: false, default: 1
       t.belongs_to :category, null: false, foreign_key: true
-      t.belongs_to :author, null: false, foreign_key: {to_table: :users}
+      t.belongs_to :author, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end
