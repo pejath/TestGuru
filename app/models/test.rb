ec99af794f80eb55e67b8b252698dbada0adde6f
pejath <pejath@yandex.ru>
2023-02-20 00:36:34 +0300

    feat: test query and seeds

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
+  def self.test_by_category_name(category_tile)
+    Test.joins('join categories on tests.category_id = categories.id')
+        .where('categories.title = ?', category_tile).order(title: :desc)
+        .pluck(:title)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create(title: 'Category 1')
+
+User.create(username: 'name', email: 'user@mail.com', role: 'user')
+admin = User.create(username: 'admin', email: 'admin@mail.com', role: 'admin')
+
+3.times do |i|
+  test = Test.create(title: "title #{i}", level: rand(1..3), category_id: Category.first.id, author_id: admin.id)
+  5.times do |j|
+    question = Question.create(body: "test #{i} question #{j}", test_id: test.id)
+    Answer.create(body: "test #{i} answer #{j}", correct: [true, false].sample, question_id: question.id)
+  end
+end
