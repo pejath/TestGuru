18c1fa2c385cb687dd7b196da1f1f0efee688f0b
pejath <pejath@yandex.ru>
2023-03-02 04:11:04 +0300

    feat: easier variant

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,20 +1,6 @@
 # frozen_string_literal: true
 
 module TestPassagesHelper
-  def result_message(test_passage)
-    if test_passage.successfully_completed?
-      content_tag(:div) do
-        content_tag(:p, 'You are successfully completed test with:') +
-          content_tag(:h2, "#{test_passage.percentage}%", color: :green)
-      end
-    else
-      content_tag(:div) do
-        content_tag(:p, 'You are failed test, your percentage was:') +
-          content_tag(:h2, "#{test_passage.percentage}%", color: :red)
-      end
-    end
-  end
-
   def current_question_helper(test_passage)
     "Current Question: #{test_passage.current_question_num} / #{test_passage.questions_count}"
   end
