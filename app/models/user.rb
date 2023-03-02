5421d6675c76526ae34163c97d2a586389de7e0f
pejath <pejath@yandex.ru>
2023-03-02 12:44:45 +0300

    feat: user auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'digest/sha1'
 class User < ApplicationRecord
   has_many :test_passages, dependent: :delete_all
   has_many :tests, through: :test_passages
@@ -11,7 +12,17 @@ class User < ApplicationRecord
     test_passages.order(id: :desc).find_by(test_id: test)
   end
 
+  def authenticate(password_string)
+    digest(password_string) == password_digest ? self : false
+  end
+
   def user_tests(level)
     tests.where(level:)
+  end
+
+  private
+
+  def digest(string)
+    Digest::SHA1.hexdigest(string)
   end
 end
