1485d9bcec337896eda584cac00c086a9504f48a
pejath <80777928+pejath@users.noreply.github.com>
2023-03-04 14:08:45 +0300

    Tg 09 (#9)
    * feat: user auth

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def flash_message(type, message)
+    content_tag :p, message, class: "flash #{type}" unless message.nil?
+  end
+
   def current_year
-    Time.now.year
+    Time.current.year
   end
 
   def github_url(author, repo)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
+require 'digest/sha1'
 class User < ApplicationRecord
   has_many :test_passages, dependent: :delete_all
   has_many :tests, through: :test_passages
   has_many :authored_tests, class_name: 'Test', foreign_key: :author_id, dependent: :nullify
 
-  validates :email, :username, presence: true
+  has_secure_password
+
+  validates :username, :email, presence: true
+  validates :email, uniqueness: { case_sensitive: false }, format: URI::MailTo::EMAIL_REGEXP
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test)
