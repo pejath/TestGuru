228de0d022f7e0d7c562fd630954eb64cb59dcba
pejath <pejath@yandex.ru>
2023-03-04 17:08:32 +0300

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def flash_message(type, message = '')
-    content_tag :p, message, class: "flash #{type}"
+  def flash_message(type, message)
+    content_tag :p, message, class: "flash #{type}" unless message.nil?
   end
 
   def current_year
