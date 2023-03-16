92bfcbb135ff38eb5062f7983aa40379082a3c81
pejath <80777928+pejath@users.noreply.github.com>
2023-03-16 11:11:12 +0300

    Task 12 (#12)
    * feat: bootstrap for user tests
    * feat: forms translation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  BOOTSTRAP_TYPE = { 'notice' => 'alert-success', 'alert' => 'alert-danger' }
+
   def flash_message(type, message)
-    content_tag :p, message, class: "flash #{type}" unless message.nil?
+    content_tag :div, message, class: "alert #{BOOTSTRAP_TYPE[type]}" unless message.nil?
   end
 
   def current_year
@@ -10,6 +12,6 @@ module ApplicationHelper
   end
 
   def github_url(author, repo)
-    link_to author, repo, target: '_blank'
+    link_to author, repo, target: '_blank', class: 'text-decoration-none text-muted'
   end
 end
