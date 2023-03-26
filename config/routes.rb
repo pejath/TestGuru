a5477853cd9c37b751f9eb269d68d4a4745f30df
pejath <pejath@yandex.ru>
2023-03-26 19:17:28 +0300

    feat: updated styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, path: :gurus, controllers: { sessions: 'sessions' },
+  devise_for :users, path: 'gurus', controllers: { sessions: 'sessions' },
              path_names: { sign_in: :login, sign_out: :logout }
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
