4ba8ba523183d4ff6d4b3c4060cff738a68b77e7
pejath <pejath@yandex.ru>
2023-03-30 13:32:34 +0300

    feat: translation and styles

---
+++ app/assets/javascripts/utilities/remove_container.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', () => {
+    const main = document.getElementById('show-question-page')
+
+    if (main) {
+        const div = document.getElementById('main-container')
+        div.classList.remove('container')
+    }
+})
